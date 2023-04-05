481468ca373dda83b40810c703f16ab418065d56
VeriSkas <92_sveto4ka@mail.ru>
2023-04-05 16:10:36 +0300

    move localSwitcher logic to useLocal hook

--- src/widgets/LocalizeSwitcher/LocalizeSwitcher.tsx
+++ src/widgets/LocalizeSwitcher/LocalizeSwitcher.tsx
@@ -1,30 +1,10 @@
-import React, { FC, useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { SupportedLanguages } from './i18n/i18n';
+import React, { FC } from 'react';
 
 import { Button } from '../../shared/components/Button/Button';
-import { LocalStorageKey } from '../../shared/constants/localStorage';
+import { useLocal } from './useLocal';
 
 export const LocalizeSwitcher: FC<{}> = () => {
-  const { i18n } = useTranslation();
-  const [language, setLanguage] = useState(
-    localStorage.getItem(LocalStorageKey.language) ?? SupportedLanguages.en
-  );
-
-  useEffect(() => {
-    if (language) {
-      void i18n.changeLanguage(language);
-    }
-  }, [i18n]);
-
-  const toggleLocal = (): void => {
-    const newLanguage =
-      language === SupportedLanguages.en ? SupportedLanguages.ru : SupportedLanguages.en;
-
-    setLanguage(newLanguage);
-    localStorage.setItem(LocalStorageKey.language, newLanguage);
-    void i18n.changeLanguage(newLanguage);
-  };
+  const toggleLocal = useLocal();
 
   return <Button onClick={toggleLocal}>&#35821;</Button>;
 };

58d293aba12d3d60a03e9f792d8eb270b1fff9f3
VeriSkas <92_sveto4ka@mail.ru>
2023-04-05 13:11:32 +0300

    change theme to default one, add transition to changing theme

--- src/shared/helpers/useTheme.tsx
+++ src/shared/helpers/useTheme.tsx
@@ -1,16 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { LocalStorageKey } from '../constants/localStorage';
 
 type Theme = 'theme1' | 'theme2';
 
 export const useTheme = (): (() => void) => {
-  const [theme, setTheme] = useState<Theme>(() => localStorage.getItem(LocalStorageKey.theme) as Theme ?? 'theme1');
+  const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const defaultTheme = isDarkTheme ? 'theme2' : 'theme1';
+  const [theme, setTheme] = useState<Theme>(
+    () => localStorage.getItem(LocalStorageKey.theme) as Theme ?? defaultTheme
+  );
 
   const toggleTheme = (): void => {
     setTheme(theme === 'theme1' ? 'theme2' : 'theme1');
   };
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const filter = `var(--filter-${theme})`;
 
     document.body.style.setProperty('--global--filter', filter);

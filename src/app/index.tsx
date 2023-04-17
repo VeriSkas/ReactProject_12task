4dd543cad5725a100e1dd5e229c80884d7ecaddc
VeriSkas <92_sveto4ka@mail.ru>
2023-04-17 13:50:36 +0300

    move App styles logic to app folder

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,17 @@ import React, { FC } from 'react';
 
 import { Routing } from './providers/router/ui';
 import { withProviders } from './providers';
+import { useThemeContext } from '../shared/helpers/useTheme';
+import classes from './styles/themes/index.module.scss';
 
-const App: FC = () => <Routing />;
+const App: FC = () => {
+  const [theme] = useThemeContext();
 
-export default withProviders(App);
+  return (
+    <div className={`${classes.App} ${classes[theme]}`}>
+      <Routing />
+    </div>
+  );
+};
+
+export default withProviders(() => <App />);
--- src/pages/StartPage/index.tsx
+++ src/pages/StartPage/index.tsx
@@ -1,20 +1,17 @@
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { useThemeContext } from '../../shared/helpers/useTheme';
 import { Header } from '../../widgets/Header/ui/Header';
 import classes from './styles.module.scss';
 
 const StartPage: FC<{}> = () => {
-  const [theme] = useThemeContext();
-
   return (
-    <div className={`${classes.App} ${classes[theme]}`}>
+    <>
       <Header />
       <div className={classes.StartPage}>
         <Outlet />
       </div>
-    </div>
+    </>
   );
 };
 
